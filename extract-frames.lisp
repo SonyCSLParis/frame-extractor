5ed532d83ab22b8aff3eba32ed9af774a2649027
remivantrijp <remi@csl.sony.fr>
2021-12-23 17:26:24 +0100

    Allowing procedural attachment for phrase-based frame extraction

--- extract-frames.lisp
+++ extract-frames.lisp
@@ -20,6 +20,26 @@
 
 (defgeneric extract-semantic-frames (utterance key &key cxn-inventory &allow-other-keys))
 
+;; Helper functions:
+;; -----------------
+(defun format-units-as-string (unit-names units)
+  "String-append the string associated with a unit, separating them by a white space."
+  (format nil "~{~a~^ ~}"
+          (mapcar #'(lambda(unit-name)
+                      (retrieve-string-for-unit unit-name units))
+                  unit-names)))
+
+(defun apply-procedural-attachment (s-expression units)
+  "Some procedure needs to be executed before fetching the strings."
+  (let ((procedure (first s-expression))
+        (body (loop for x in (rest s-expression)
+                    collect (if (symbolp x) x (apply-procedural-attachment x units)))))
+    (case procedure
+      (unit-append (format-units-as-string body units))
+      (unit-remove (format-units-as-string (remove (first body) (second body)) units))
+      (t
+       s-expression))))
+
 ;; 1/ PHRASE-BASED:
 ;; Method that only cares about the strings. This relies on the use of BOUNDARIES.
 ;; ----------------------------------------------------------------------------------------------------------------------------
@@ -27,21 +47,25 @@
   (= 1 (- (third boundary) (second boundary))))
 
 (defun retrieve-string-for-unit (unit-name units)
-  (let* ((boundaries (fcg-get-boundaries units))
-         (boundary-spec (assoc unit-name boundaries))
-         (strings-in-root (extract-string (get-root units)))
-         (lexical-boundaries (loop for boundary in boundaries
-                                   when (and (= 1 (- (third boundary) (second boundary)))
-                                             (>= (second boundary) (second boundary-spec))
-                                             (<= (third boundary) (third boundary-spec)))
-                                   collect boundary))
-         (strings (loop for boundary in lexical-boundaries
-                        for string-spec = (let ((strng (find (first boundary) strings-in-root :key #'second)))
-                                            (if strng (list strng)
-                                              (extract-string (assoc (first boundary) units))))
-                        when string-spec
-                        append (mapcar #'third string-spec))))
-    (format nil "~{~a~^ ~}" strings)))
+  (cond ((variable-p unit-name) "UNK")
+        ((listp unit-name) (apply-procedural-attachment unit-name units))
+        ((stringp unit-name) unit-name)
+        (t
+         (let* ((boundaries (fcg-get-boundaries units))
+                (boundary-spec (assoc unit-name boundaries))
+                (strings-in-root (extract-string (get-root units)))
+                (lexical-boundaries (loop for boundary in boundaries
+                                          when (and (= 1 (- (third boundary) (second boundary)))
+                                                    (>= (second boundary) (second boundary-spec))
+                                                    (<= (third boundary) (third boundary-spec)))
+                                          collect boundary))
+                (strings (loop for boundary in lexical-boundaries
+                               for string-spec = (let ((strng (find (first boundary) strings-in-root :key #'second)))
+                                                   (if strng (list strng)
+                                                     (extract-string (assoc (first boundary) units))))
+                               when string-spec
+                               append (mapcar #'third string-spec))))
+           (format nil "~{~a~^ ~}" strings)))))
 
 (defmethod extract-semantic-frames ((utterance string)
                                     (key (eql :phrase-based))
@@ -80,6 +104,11 @@
 ;; 2/ MEANING-BASED:
 ;; Method for grammars that assume that semantic frames are represented as part of the meaning of the utterance.
 ;; ----------------------------------------------------------------------------------------------------------------------------
+(defun find-unit-with-predicate (predicate units)
+  (loop for unit in units
+        when (find predicate (unit-feature-value unit 'meaning) :test  #'equal)
+        return unit))
+
 (defmethod extract-semantic-frames ((utterance string)
                                     (key (eql :meaning-based))
                                     &key cxn-inventory frame-dev-monitor
@@ -93,12 +122,24 @@
                               collect (list predicate frame-elements)))
            (sem-frames (loop for frame-type in frame-types
                              for var = (second (first frame-type))
-                             collect (loop for fe in (second frame-types)
-                                           for element = (loop for predicate in meaning
-                                                               when (and (unify fe (first predicate) +no-bindings+)
-                                                                         (eql (second predicate) var))
-                                                               collect predicate)
-                                           append element))))
-      (list frame-types sem-frames))))
-;; (extract-semantic-frames "The wind caused damage." :meaning-based :cxn-inventory *fcg-english*)
+                             collect (make-instance (first (first frame-type))
+                                                    :evoked-by (retrieve-string-for-unit
+                                                                (unit-name (find-unit-with-predicate (first frame-type)
+                                                                                                     (fcg-get-transient-unit-structure final-node)))
+                                                                (fcg-get-transient-unit-structure final-node))
+                                                    :frame-elements (loop for fe in (second frame-type)
+                                                                          for element = (loop for predicate in meaning
+                                                                                              when (and (string= (symbol-name fe) 
+                                                                                                                 (symbol-name (first predicate)))
+                                                                                                        (eql (second predicate) var))
+                                                                                              return predicate)
+                                                                          when element
+                                                                          collect (list (first element) (third element)))))))
+      ;; To be replaced by a real web monitor.
+      (when frame-dev-monitor
+        (loop for sem-frame in sem-frames
+              do (add-element (make-html sem-frame :expand-initially t))))
+      sem-frames)))
+;; (extract-semantic-frames "The wind caused damage." :meaning-based :cxn-inventory *fcg-english* :frame-dev-monitor t)
+
 ;; (comprehend "The wind caused damage." :cxn-inventory *fcg-english*)
